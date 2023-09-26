d627ab25d16f6726ccfb60ae2ad5d4eb081bb254
Alain-Kay <kazadikayumba@gmail.com>
2023-09-26 13:43:27 +0200

    Rename the models

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Customers extends Model
+class Customer extends Model
 {
     use HasFactory;
 
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Products extends Model
+class Product extends Model
 {
     use HasFactory;
 
