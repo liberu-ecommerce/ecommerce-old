e7b34802ec926a51bf5fe0d39fe8cb41c7211be0
Curtis Delicata <curtis.delicata@outlook.com>
2023-09-26 10:33:03 +0000

    Work in progress, validation requests for customer,product,order

---
+++ app/Http/Requests/ValidateProductRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ValidateProductRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        $this->route('product');
+
+        return [
+
+            'group' => 'required|max:50',
+            'gid' => 'required|max:50',
+            'name' => 'required|max:50',
+            'addr_id' => 'required|max:50',
+            'rin' => 'required|max:50',
+            'rfn' => 'required|max:50',
+            'lang' => 'required|max:50',
+            'phon' => 'required|max:50',
+        ];
+    }
+}
