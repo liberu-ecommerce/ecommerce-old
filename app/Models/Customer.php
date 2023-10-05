4f636e5d8e9d853b242bacbf70c7e46b4e2dfe2e
Alain-Kay <kazadikayumba@gmail.com>
2023-10-05 21:29:08 +0200

    Added Model and Migration Review_rating

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -31,4 +31,9 @@ class Customer extends Model
     {
         return $this->hasMany(Invoice::class);
     }
+
+    public function reviewRatings()
+    {
+        return $this->hasMany(ReviewRating::class);
+    }
 }
